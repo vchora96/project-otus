0f9065437f45fe5c9160137fe170a9e91513484f
andrey.oshkin <vchora96@yandex.ru>
2022-05-15 22:27:56 +0300

    add card info selectors

--- src/main/java/ru/oshkin/pages/CardInfoPage.java
+++ src/main/java/ru/oshkin/pages/CardInfoPage.java
@@ -1,4 +1,22 @@
 package ru.oshkin.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class CardInfoPage {
+
+    @FindBy(css = ".course-header2__title")
+    private WebElement title; //Название
+
+    @FindBy(css = ".course-header2__subtitle")
+    private WebElement subtitle; //Описание
+
+    @FindBy(css = "course-header2-bottom__content-items > div:nth-child(1) div.course-header2-bottom__content-item-text" +
+            " p.course-header2-bottom__item-text")
+    private WebElement durationCourse; // Продолжительность курса
+
+    @FindBy(css = "course-header2-bottom__content-items > div:nth-child(2) div.course-header2-bottom__content-item-text" +
+            " p.course-header2-bottom__item-text")
+    private WebElement format; // Формат
+
 }
--- src/main/java/ru/oshkin/pages/TestingCoursesInfoPage.java
+++ src/main/java/ru/oshkin/pages/TestingCoursesInfoPage.java
@@ -1,4 +1,16 @@
 package ru.oshkin.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
 public class TestingCoursesInfoPage {
+    @FindBy(css = "div[id='categories_id'] a[title='Тестирование']")
+    private WebElement buttonTesting;
+
+    @FindBy(css = "div.lessons a")
+    private List<WebElement> lessons;
+
+
 }
